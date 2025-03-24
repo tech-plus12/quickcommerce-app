d9c1482378cdf5cf22536a812cdd6395f4824155
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2025-03-24 16:49:51 +0530

    .env setup done

--- App.jsx
+++ App.jsx
@@ -5,9 +5,13 @@ import { Provider } from "react-redux";
 import { store } from "./src/store/store";
 import { CartProvider } from "./src/context/CartContext";
 import { useEffect } from "react";
+import Config from "react-native-config";
+
+const apiUrl = Config.API_URL;
 
 const App = () => {
   useEffect(() => {
+    console.log(`API URL is ${apiUrl}`);
     fetch("http://192.168.1.18:3001/api/test")
       .then((response) => response.json())
       .then((data) => console.log(data))
